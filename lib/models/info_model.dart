7835749f298be5a86dd2ad974e70a0b4d7609bd3
Ayman Salah <aymancoom3@gmail.com>
2025-10-04 20:16:48 +0300

    Folders Structure and assets

---
+++ lib/models/info_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class InfoModel {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  InfoModel({required this.title, required this.subtitle, required this.icon});
+}
